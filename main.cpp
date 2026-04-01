43fe2c58659143a78efac5ba711eb49b5919da91
AndrewAZah <andrew@roguesurge.com>
2026-04-01 16:17:08 -0700

    Q41 done, file not found for tests though

--- main.cpp
+++ main.cpp
@@ -20,8 +20,30 @@ int main()
 
     // TODO
 
-    cout << left << setprecision(2) << fixed;
-    cout << "The area is " << area << endl;
+    if (choice == 1){
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
+        area = PI * pow(radius, 2);
+    } else if (choice == 2){
+        cout << "Enter the width of the rectangle: ";
+        cin >> width;
+        cout << "Enter the height of the rectangle: ";
+    } else if (choice == 3){
+        cout << "Enter the base of the triangle: ";
+        cin >> base;
+        cout << "Enter the height of the triangle: ";
+        cin >> height;
+        area = 0.5 * base * height;
+    } else if (choice == 4){
+        cout << "Program Stopped";
+    } else {
+        cout << "Wrong number";
+    }
+    
+    if ((choice >= 1) && (choice <= 3)){
+        cout << left << setprecision(2) << fixed;
+        cout << "The area is " << area << endl;
+    }
 
     return 0;
 }
